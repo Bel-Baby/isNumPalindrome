bfdcb6f80616248d919f57b5bd8f3f3c59227bc6
Abel Amuta <amutaprincedavid@gmail.com>
2024-04-10 12:32:19 +0100

    refactored

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 //Given a positive integer, write a function that returns true if the given number is a palindrome, else false. For example, 12321 is a palindrome, but 1451 is not a palindrome.
 
-const int = document.querySelector("#int");
 const check = document.querySelector("#check");
-const output = document.querySelector("#output");
 
-const checkForPalindrome = () => {
-    const intVal = int.value ;
-    const reversedInt = intVal.split("").reverse().join("");
-    const isIntPalindromeOrNot = reversedInt === intVal ? "true" : "false";
-
-    output.textContent = isIntPalindromeOrNot;
+const reversedInt = () => {
+    const reversedInt = int.value.split("").reverse().join("");
+    return reversedInt;
 }
 
-check.addEventListener("click", checkForPalindrome);
+const checkForPalindrome = (intVal) => {
+    const reversedIntVal = reversedInt();
+    if (reversedIntVal === intVal) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const runPalindomechecker = () => {
+    const int = document.querySelector("#int");
+    const output = document.querySelector("#output");
+    output.textContent = checkForPalindrome(int.value).toString();
+}
+
+check.addEventListener("click", runPalindomechecker);
